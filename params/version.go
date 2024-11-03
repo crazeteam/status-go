e938635d045099dbeb8dbbaf8ddb7d7ba4805db3
Igor Sirotin <sirotin@status.im>
2024-11-03 00:47:15 +0000

    chore_: get version with go generate (#6014)
    * chore_: get version with go generate
    
    * fix_: test-functional
    
    * fix_: trip space in version and gitcommit
    
    * fix_: TestUserAgent

--- params/version.go
+++ params/version.go
@@ -1,11 +1,39 @@
 package params
 
-// Version is defined in git tags.
-// We set it from the Makefile.
-var Version string
+import (
+	_ "embed"
+	"strings"
+)
 
-// GitCommit is a commit hash.
-var GitCommit string
+// Use go:generate script to get the version and git commit.
+// VERSION and GIT_COMMIT files are used in further `go:embed` commands to load values to the variables.
+// Suppress errors, assuming files have already been properly generated. Required for Docker builds.
+//go:generate sh -c "../_assets/scripts/version.sh > VERSION || true"
+//go:generate sh -c "git rev-parse --short HEAD > GIT_COMMIT || true"
+
+var (
+	// version is defined in git tags.
+	// We set it from the Makefile.
+	//go:embed VERSION
+	version string
+
+	// gitCommit is a commit hash.
+	//go:embed GIT_COMMIT
+	gitCommit string
+)
 
 // IpfsGatewayURL is the Gateway URL to use for IPFS
-var IpfsGatewayURL string
+const IpfsGatewayURL = "https://ipfs.status.im/"
+
+func init() {
+	version = strings.TrimSpace(version)
+	gitCommit = strings.TrimSpace(gitCommit)
+}
+
+func Version() string {
+	return version
+}
+
+func GitCommit() string {
+	return gitCommit
+}
